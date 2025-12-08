28b47a28245534ea988a600d4677e859c73cb070
Luca Mathuse <mathuse.luca@icloud.com>
2025-12-08 09:17:26 +0100

    fixed headline spacing in contact component

--- app/components/Contact.vue
+++ app/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="contact" class="wrapper my-24">
-    <h2 class="mb-6"><slot></slot></h2>
+    <h2 class="mb-4"><slot></slot></h2>
     <div class="flex flex-col flex-1 gap-6">
       <div class="text">
         <p>Hast du eine Idee, ein Projekt oder möchtest du einfach ins Gespräch kommen? Ich freue mich über jede Nachricht. Schreib mir gerne, ich antworte so schnell ich kann! </p>
